98dc5574b3a925b920238b186b3c70624806f603
Olga Sukhareva <3830921@gmail.com>
2025-03-26 14:56:33 +0300

    More objects

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,14 @@ public class Main {
         double heightM = 1.87;
         System.out.println("Килограмм: " + weightKilo + " Сантиметров: " + heightM);
         int bmi = service.calculate(heightM, weightKilo);
-        System.out.println(bmi);
+        System.out.println("BMI: " + bmi);
+
+        weightKilo = 65;
+        heightM = 1.67;
+        System.out.println("Килограмм: " + weightKilo + " Сантиметров: " + heightM);
+        bmi = service.calculate(heightM, weightKilo);
+        System.out.println("BMI: " + bmi);
+        
 
     }
 }
